09feddfe5ef40c501fd3997f585a2a6499fe32d5
Tagrand <thomas.a.grand@gmail.com>
2018-01-26 11:15:45 +0000

    add bad signup test

--- spec/features/signup_and_submit_spec.rb
+++ spec/features/signup_and_submit_spec.rb
@@ -9,10 +9,27 @@ feature 'Signup and submit' do
     expect{ sign_up }.to change(User, :count). by(1)
   end
 
+  scenario 'User gives wrong confirmation password' do
+    expect{ bad_sign_up }.not_to change(User, :count)
+  end
+
+  scenario 'User gives wrong confirmation password' do
+    bad_sign_up
+    expect(page).not_to have_content("a.pourian@hotmail.co.uk")
+  end
+
   def sign_up
     visit '/signup'
     fill_in('email', :with => 'b.pourian@hotmail.co.uk')
     fill_in('password', :with => 'password1')
+    click_button 'submit'
+  end
+
+  def bad_sign_up
+    visit '/signup'
+    fill_in('email', :with => 'a.pourian@hotmail.co.uk')
+    fill_in('password', :with => 'password1')
+    fill_in('password confirmation', :with => 'password2')
     click_button 'submit'
   end
 end
